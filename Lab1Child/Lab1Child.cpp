053c25e20a57b92b47e4ba9dbbd09c4844ea2d67
Neustod <vane90989@gmail.com>
2024-10-02 19:22:33 +0300

    Project files were added.

---
+++ Lab1Child/Lab1Child.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+// Функция перевода из двоичной ПСС в десятичную ПСС
+int convert(const string& str) 
+{
+	int sum = 0;
+	int strLen = str.size();
+
+	for (int i = 0; i < strLen; i++) 
+	{
+		int discharge = strLen - 1 - i;
+		sum += (str[i] -'0') * pow(2, discharge);
+	}
+
+	return sum;
+}
+
+
+// Основная нить дочернего процесса.
+int main(int argc, char* argv[])
+{
+	cout << "In process provided args: " << argc << endl;
+
+	char* arg = argv[0];
+	string str{ arg };
+
+	int result = convert(str);
+
+	cout << "Result is: " << result << endl;
+
+	system("pause");
+	return 0;
+}
